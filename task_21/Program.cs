9ed8c24bf9b772816fc382182e2d9647b3156174
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-09-01 21:04:32 +0300

    task_21

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -10,5 +10,119 @@ Console.Write("Укажите координаты точки B: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
 int y2 = Convert.ToInt32(Console.ReadLine());
 int z2 = Convert.ToInt32(Console.ReadLine());
+// корень(x2 - x1)^ + (y2 - y1)^ + (z2 - z1)^ 
+
+//d - расстояние между точками в 2D пространстве
+// // d = корень из выражения((x2-x1)^2 + (y2-y1)^2)
+// //Math.Sqrt(число) => корень из числа: Math.Sqrt(25) => 5
+// double d = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
+// //Math.Rount(d) => d: Math.Rount(1.275) => 1
+// Console.WriteLine(Math.Round(d, 3));
+// Console.WriteLine($"d = {d:f2}");// срез
+// // {$переменная: fсколько знаков}
+double d = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2) + Math.Pow(z2 - z1,2));
+Console.WriteLine(Math.Round(d, 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
